ecec11082e9f5a947edddcc9bdb6ddfeda1c8b22
Roman Kazachkov <roman.kazachkov1@gmail.com>
2025-10-26 18:47:16 +0300

    Fix 1

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,10 @@ use std::io;
 
 fn main() {
     let mut path = String::new();
-
-    if io::stdin().read_line(&mut path).is_err() {
-        println!("failure");
-        return;
-    }
-
+    io::stdin().read_line(&mut path).unwrap();
     let path = path.trim();
 
-    if fs::read_to_string(path).is_ok() {
+    if fs::metadata(path).is_ok() {
         println!("success");
     } else {
         println!("failure");
